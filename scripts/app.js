56e687072cb2a7d77a1205341386006f629868d9
yooperjb <jmb181@humboldt.edu>
2020-11-16 11:47:38 -0800

    initial map setup

---
+++ scripts/app.js
@@ -0,0 +1,13 @@
+
+  require(["esri/Map", "esri/views/MapView"], function (Map, MapView) {
+    var map = new Map({
+      basemap: "topo-vector"
+    });
+
+    var view = new MapView({
+      container: "viewDiv",
+      map: map,
+      center: [-118.805, 34.027], // longitude, latitude
+      zoom: 13
+    });
+  });
